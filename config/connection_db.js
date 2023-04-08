b071e585442786afa109a9700a036a4707413feb
AWIKSARAH <awiksarah@gmail.com>
2023-04-08 16:14:35 +0300

    Fix the .env Example

--- config/connection_db.js
+++ config/connection_db.js
@@ -7,7 +7,8 @@ const connection = async () => {
   try {
     const URL_DB = `mongodb+srv://${process.env.USERNAME_DB}:${process.env.PASSWORD}@cluster0.jdjzk6x.mongodb.net/?retryWrites=true&w=majority`;
 console.log(URL_DB);
-    await mongoose.connect(process.env.URL_DB, {
+
+    await mongoose.connect(URL_DB, {
       useUnifiedTopology: true,
       dbName: process.env.DB_NAME
     });
