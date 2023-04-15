3884bb3e0ad85152cd64f39ebda8f3b5b88c5114
ayadtarraf <tarraf_tarraf1111@hotmail.com>
2023-04-15 17:34:33 +0300

    blog and news

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors"
-import { handle404Error, handleErrors } from "./HandlingError/HandleError.js";
+// import { handle404Error, handleErrors } from "./HandlingError/HandleError.js";
 import connection from './config/connection_db.js';
+import newsletter from './routes/route_news.js'
+import blog from './routes/route-blog.js'
 const app = express();
 const port = process.env.PORT || 3000;
 connection();
@@ -18,10 +20,12 @@ app.use(express.static("public"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use("/dashboard/news",newsletter);
+app.use("/dashboard/blog",blog);
+app.use('/public/images',express.static('./public/images'))
 //Handling Errors 404 and other
-app.use(handle404Error);
-app.use(handleErrors);
+// app.use(handle404Error);
+// app.use(handleErrors);
 
 app.listen(port, () => {
   console.log(`Hello :) Your Server Running on PORT: ${port}`);
---
+++ models/models_blog.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+const {Schema, model} = mongoose;
+const Blogschema = new Schema({
+    // tag_id: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: "tag",
+    // },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+
+    }
+
+
+},
+    {
+        collection: "Blog"
+    }
+)
+    ;
+// Blogschema.pre(["find", "findOne", "save", "create"], function () {
+//     this.populate(["tag_id"]);
+// });
+
+const blogschema = model("Blog", Blogschema);
+export default blogschema;
