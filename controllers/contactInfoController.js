4347302bf4d9ee46091b20418afbe8685d838808
AWIKSARAH <awiksarah@gmail.com>
2023-04-17 10:37:04 +0300

    Finish The Info

--- controllers/contactInfoController.js
+++ controllers/contactInfoController.js
@@ -1,9 +1,10 @@
-import contactModel from "../models/contactInfoModel.js";
 
+import contactModel from "../models/contactInfoModel.js";
+import {deleteImage} from '../middleware/HandlingImage.js'
 export const create = (req, res, next) => {
 
   const { socialMedia, aboutUs } = req.body;
-  const logo = req.body.contact;
+  const logo = req.file.path;
   const social = contactModel.create({
     socialMedia: socialMedia,
     logo: logo,
@@ -12,8 +13,6 @@ export const create = (req, res, next) => {
   social
     .then((ress) => {
       if (!ress) {
-        // const err = new Error(`Error creating contact`);
-        // err.status = 404;
       res.status(500).json({ success: false, error: err });
       }
       return res.status(200).json({ success: true, data: ress });
@@ -42,3 +41,38 @@ export const get = (req, res, next) => {
       return next(err);
     });
 };
+
+
+/** 
+ * Functioon to update a contact with Information 
+ */
+export const updateContact = async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const oldContact = await contactModel.findOne({_id:_id});
+
+    if (!oldContact) {
+      return res.status(404).json({ success: false, error: "Contact not found" });
+    }
+
+    const { socialMedia, aboutUs } = req.body;
+    const logo = req.file.path || oldContact.logo;
+
+    console.log(oldContact.logo);
+    if (logo) {
+      const imagePath = oldContact.logo
+      deleteImage(`${imagePath}`);
+    } else{console.log('nonono');}
+
+    const updatedContact = await contactModel.findByIdAndUpdate(
+      oldContact._id,
+      { socialMedia, logo, aboutUs },
+      { new: true }
+    );
+
+    return res.status(200).json({ success: true, data: updatedContact });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ success: false, error });
+  }
+}
--- models/contactInfoModel.js
+++ models/contactInfoModel.js
@@ -3,40 +3,40 @@ import mongoose from "mongoose";
 const contactInfo = new mongoose.Schema({
 
     // socialMedia: {
-    //     type: [
-    //       {
-    //         name: {
-    //           type: String,
-    //           // required: true,
-    //           minlength: [
-    //             2,
-    //             "Social media name must be at least 2 characters long",
-    //           ],
-    //           maxlength: [100, "Social media name must not exceed 100 characters"],
-    //         },
-    //         url: {
-    //           type: String,
-    //           // required: true,
-    //           // minlength: [2, "Social media url must be at least 2 characters long"],
-    //           // maxlength: [500, "Social media url must not exceed 500 characters"],
-    //         },
-    //       },
-    //     ],
-    //     validate: {
-    //       validator: function (v) {
-    //         return v && v.length > 0;
-    //       },
-    //       message: "At least one social media account is required",
-    //     },
-    //   },
+      //   type: [
+      //     {
+      //       name: {
+      //         type: String,
+      //         required: true,
+      //         minlength: [
+      //           2,
+      //           "Social media name must be at least 2 characters long",
+      //         ],
+      //         maxlength: [100, "Social media name must not exceed 100 characters"],
+      //       },
+      //       url: {
+      //         type: String,
+      //         required: true,
+      //         minlength: [2, "Social media url must be at least 2 characters long"],
+      //         maxlength: [500, "Social media url must not exceed 500 characters"],
+      //       },
+      //     },
+      //   ],
+      //   validate: {
+      //     validator: function (v) {
+      //       return v && v.length > 0;
+      //     },
+      //     message: "At least one social media account is required",
+      //   },
+      // },
     logo:{
         type:String,
-        // required: true
+        required: true
     },
     aboutUs: {
         type: String,
         // required: true
-    }
+    },
 })
 
-export default mongoose.model('socialMedia',contactInfo);
+export default mongoose.model('contactInfo', contactInfo, 'contactInfo');
