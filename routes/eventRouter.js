9a35ef60ce4987837080182e1906311062b8674e
AWIKSARAH <awiksarah@gmail.com>
2023-04-28 13:23:16 +0300

    fixing the route

--- routes/eventRouter.js
+++ routes/eventRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 import controller from "../controllers/eventController.js";
+import uploadImage from '../middleware/imageHandlerMiddleware.js'
 
 const router = express.Router();
 
-router.post("/", controller.add);
+router.post("/",uploadImage('event'), controller.add);
 
 router.get("/all/:type?", controller.getAll);
 router.get("/conf/:type?", controller.getPrivateEvent);
@@ -11,7 +12,7 @@ router.get('/latest', controller.latestPlace)
 router.get("/:id", controller.getById);
 
 router.patch("/confirm/:id", controller.updateConfirmationById);
-router.patch("/update/:id", controller.edit);
+router.patch("/update/:id", uploadImage('event'),controller.edit);
 
 router.delete("/Delete/:id", controller.Delete);
 router.delete("/Delete/sure/yes", controller.deleteAll);
