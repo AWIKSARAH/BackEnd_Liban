09f5ea6a68b4bfb55724f37b5424407d7a11a8ad
AWIKSARAH <awiksarah@gmail.com>
2023-05-05 19:21:38 +0300

    done

---
+++ routes/eventRouter 2.js
@@ -0,0 +1,20 @@
+// import express from "express";
+// import controller from "../controllers/eventController.js";
+// import uploadImage from '../middleware/imageHandlerMiddleware.js'
+
+// const router = express.Router();
+
+// router.post("/",uploadImage('event'), controller.add);
+
+// router.get("/all/:type?", controller.getAll);
+// router.get("/conf/:type?", controller.getPrivateEvent);
+// router.get('/latest', controller.latestPlace)
+// router.get("/:id", controller.getById);
+
+// router.patch("/confirm/:id", controller.updateConfirmationById);
+// router.patch("/update/:id", uploadImage('event'),controller.edit);
+
+// router.delete("/Delete/:id", controller.Delete);
+// router.delete("/Delete/sure/yes", controller.deleteAll);
+
+// export default router;
